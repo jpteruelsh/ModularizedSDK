f065c1723331359680b26a50d40e6b66b84b6f0a
John Patrick Teruel <jpteruel95@gmail.com>
2024-08-06 02:11:28 +0800

    chore: Added npmPublish to the buld.gradle.kts of the Android demo project to fix build errors

--- ModularizedAndroidDemo/build.gradle.kts
+++ ModularizedAndroidDemo/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.npmPublish) apply false
 }
